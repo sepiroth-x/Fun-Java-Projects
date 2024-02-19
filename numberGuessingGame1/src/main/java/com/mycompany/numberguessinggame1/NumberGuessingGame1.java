99183fac56a19896fe6c6ab02383531456e31a19
sepiroth-x <45693885+sepiroth-x@users.noreply.github.com>
2024-02-19 19:23:39 +0800

    Started

---
+++ numberGuessingGame1/src/main/java/com/mycompany/numberguessinggame1/NumberGuessingGame1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.numberguessinggame1;
+
+/**
+ *
+ * @author 东南大侠
+ */
+public class NumberGuessingGame1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
